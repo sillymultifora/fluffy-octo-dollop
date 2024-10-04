882e649c63204941bc5b299dfdbdb9fa1ff780b2
None <sillymultifora@gmail.com>
2024-10-04 21:10:59 +0200

    handle process kill

--- start_servers.sh
+++ start_servers.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 # Start VLLM server in the background
-vllm serve NousResearch/Hermes-3-Llama-3.1-8B --api-key my_token --dtype auto  &
+vllm serve NousResearch/Hermes-3-Llama-3.1-8B --tensor-parallel-size 2  --api-key my_token --dtype auto  &
+SERVER_VLMM_PID=$!
 
 # Start processing server in the background
 python processing_server.py --api-key my_token --api-base http://localhost:8000/v1/ --model-name NousResearch/Hermes-3-Llama-3.1-8B &
+SERVER_PROCESSING_PID=$!
 
-# Wait for any process to exit
-wait -n
+function terminate_servers {
+    echo "Terminating servers..."
+    kill $SERVER_VLMM_PID
+    kill $SERVER_PROCESSING_PID
+    wait $SERVER_VLMM_PID
+    wait $SERVER_PROCESSING_PID
+    exit 0
+}
 
-# Exit with status of process that exited first
-exit $?
+trap terminate_servers SIGINT SIGTERM
+
+# Wait for both process to finish
+wait $SERVER_VLMM_PID
+wait $SERVER_PROCESSING_PID
