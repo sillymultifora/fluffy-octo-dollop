2f648392f533ea3beb40ed83b9379d8da545fad1
None <sillymultifora@gmail.com>
2024-10-04 20:13:04 +0200

    upd dockerfile

--- dockerfile
+++ dockerfile
@@ -1,11 +1,11 @@
-FROM pytorch/pytorch:2.4.1-cuda12.1-cudnn9-devel
+FROM pytorch/pytorch:2.4.0-cuda12.1-cudnn9-devel
 
 RUN apt-get update && \
     apt-get upgrade -y && \
     apt-get install -y git
-
+    
 RUN git clone https://github.com/sillymultifora/fluffy-octo-dollop.git
 RUN cd fluffy-octo-dollop
 RUN pip install -r requirments.txt
 
-CMD ./start_servers.sh
+CMD bash ./start_servers.sh
--- start_servers.sh
+++ start_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start VLLM server in the background
-vllm serve NousResearch/Hermes-3-Llama-3.1-8B tensor-parallel-size 3 --api-key my_token --dtype auto  &
+vllm serve NousResearch/Hermes-3-Llama-3.1-8B --api-key my_token --dtype auto  &
 
 # Start processing server in the background
 python processing_server.py --api-key my_token --api-base http://localhost:8000/v1/ --model-name NousResearch/Hermes-3-Llama-3.1-8B &
